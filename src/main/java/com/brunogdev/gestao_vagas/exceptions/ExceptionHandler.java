911731de525c8b58d52ed677712a0d0b67285684
Bruno Gomes <bruno.azevedo@edu.ufes.br>
2023-12-23 19:55:43 -0300

    validation and exception error handler

--- src/main/java/com/brunogdev/gestao_vagas/exceptions/ExceptionHandler.java
+++
@@ -1,5 +0,0 @@
-package com.brunogdev.gestao_vagas.exceptions;
-
-public class ExceptionHandler {
-
-}
--- src/main/java/com/brunogdev/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/brunogdev/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -3,6 +3,7 @@ package com.brunogdev.gestao_vagas.modules.candidate;
 import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -12,13 +13,14 @@ public class CandidateEntity {
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "This Field [username] no need to have space")
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "This Field [username] can not have space")
     private String username;
 
     @Email(message = "This Field needs to contain a valid email")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "The password needs to have minimun 10 characters")
     private String password;
     private String description;
     private String curriculum;
