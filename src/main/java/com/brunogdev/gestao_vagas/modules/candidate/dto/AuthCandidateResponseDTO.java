1332a53968d058937af79e129a849caec773b844
Bruno Gomes <bruno.azevedo@edu.ufes.br>
2024-04-29 22:51:05 -0300

    Controller para autenticar user

--- src/main/java/com/brunogdev/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
+++ src/main/java/com/brunogdev/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -1,5 +1,6 @@
 package com.brunogdev.gestao_vagas.modules.candidate.dto;
 
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,4 +13,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AuthCandidateResponseDTO {
     private String access_token;
+    private Long expires_in;
 }
