1036436a96e946492cec8fd2c9b35309fa242770
Bruno Gomes <bruno.azevedo@edu.ufes.br>
2024-04-29 21:23:33 -0300

    Created token from candidate

---
+++ src/main/java/com/brunogdev/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,55 @@
+package com.brunogdev.gestao_vagas.modules.candidate.useCases;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import javax.naming.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.brunogdev.gestao_vagas.modules.candidate.CandidateRepository;
+import com.brunogdev.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import com.brunogdev.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+import org.springframework.beans.factory.annotation.Value;
+
+
+@Service
+public class AuthCandidateUseCase {
+
+    @Value("${security.token.secret.candidate}")
+    private String secretkey;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO)
+            throws AuthenticationException {
+        var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("Username/password incorrect");
+                });
+
+        var passwordMatches = this.passwordEncoder.matches(authCandidateRequestDTO.password(),
+                candidate.getPassword());
+
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretkey);
+        var token = JWT.create().withIssuer("javagas").withSubject(candidate.getId().toString())
+                .withClaim("roles", Arrays.asList("candidate"))
+                .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+                .withClaim("roles", Arrays.asList("candidate")).sign(algorithm);
+
+        var authCandidateResponse = AuthCandidateResponseDTO.builder().access_token(token).build();
+
+        return authCandidateResponse;
+    }
+}
