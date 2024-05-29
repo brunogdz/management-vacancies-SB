179c88643ac2280ab3939f14c955d90e5b6d9db2
Bruno Gomes <bruno.azevedo@edu.ufes.br>
2024-05-29 02:18:08 -0300

    auth para company

--- src/main/java/com/brunogdev/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/com/brunogdev/gestao_vagas/modules/company/controllers/JobController.java
@@ -2,6 +2,7 @@ package com.brunogdev.gestao_vagas.modules.company.controllers;
 
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/job")
+@RequestMapping("/company/job")
 public class JobController {
 
     @Autowired
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
+    @PreAuthorize("hasRole('COMPANY')")
     public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO,
             HttpServletRequest request) {
         var companyId = request.getAttribute("company_id");
--- src/main/java/com/brunogdev/gestao_vagas/modules/company/userCases/AuthCompanyUseCase.java
+++ src/main/java/com/brunogdev/gestao_vagas/modules/company/userCases/AuthCompanyUseCase.java
@@ -1,6 +1,7 @@
 package com.brunogdev.gestao_vagas.modules.company.userCases;
 
 import java.time.Instant;
+import java.util.Arrays;
 import javax.naming.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.brunogdev.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.brunogdev.gestao_vagas.modules.company.dto.AuthCompanyResponseDTO;
 import com.brunogdev.gestao_vagas.modules.company.repositories.CompanyRepository;
 import java.time.Duration;
 
@@ -25,7 +27,8 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public AuthCompanyResponseDTO execute(AuthCompanyDTO authCompanyDTO)
+            throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername())
                 .orElseThrow(() -> {
                     throw new UsernameNotFoundException("Username / password incorrect");
@@ -40,10 +43,16 @@ public class AuthCompanyUseCase {
         }
 
         Algorithm algorithm = Algorithm.HMAC256(secretkey);
+
+        var expires_in = Instant.now().plus(Duration.ofHours(2));
+
         var token = JWT.create().withIssuer("javagas")
                 .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
-                .withSubject(company.getId().toString()).sign(algorithm);
+                .withSubject(company.getId().toString()).withExpiresAt(expires_in)
+                .withClaim("roles", Arrays.asList("COMPANY")).sign(algorithm);
 
-        return token;
+        var authCompanyResponseDTO = AuthCompanyResponseDTO.builder().access_token(token)
+                .expires_in(expires_in.toEpochMilli()).build();
+        return authCompanyResponseDTO;
     }
 }
